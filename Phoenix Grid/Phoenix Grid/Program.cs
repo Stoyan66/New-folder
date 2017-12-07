f1a450aa9480787db5fd695fb7a0ab05c3a1dc72
Stoyan66 <bgstoencha@gmail.com>
2017-12-07 15:59:24 +0200

    Add files via upload

---
+++ Phoenix Grid/Phoenix Grid/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Phoenix_Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"^([^\s_]{3})(\.[^\s_]{3})*$";
+
+            while (input != "ReadMe")
+            {
+                var inputMatch = Regex.Match(input, pattern);
+                if (inputMatch.Success)
+                {
+                    if (Palindrome(input))
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+        static bool Palindrome(string word)
+        {
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length -1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
