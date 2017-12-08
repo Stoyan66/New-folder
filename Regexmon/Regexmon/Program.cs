98aeb1452b40d73d3c717dc45fb66064e705a5b8
Stoyan66 <bgstoencha@gmail.com>
2017-12-08 08:36:07 +0200

    Add files via upload

---
+++ Regexmon/Regexmon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regexmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string didimonPattern = @"([^A-Za-z-]+)";
+            string bojoPattern = @"([A-Za-z]+\-[A-Za-z]+)";
+
+            while (true)
+            {
+                var didimonMatch = Regex.Match(input, didimonPattern);
+
+
+                if (didimonMatch.Success)
+                {
+                    Console.WriteLine(didimonMatch.Value);
+                    input = input.Substring(input.IndexOf(didimonMatch.Value) + didimonMatch.Value.Length);
+                }
+                else
+                {
+                    break;
+                }
+
+                var bojoMatch = Regex.Match(input, bojoPattern);
+
+                if (bojoMatch.Success)
+                {
+                    Console.WriteLine(bojoMatch.Value);
+                    input = input.Substring(input.IndexOf(bojoMatch.Value) + bojoMatch.Value.Length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
+
+
